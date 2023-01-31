5e84e587fba10bfa5a30a7132fb50a6b984fce48
Jose IV <ivjosesantos@gmail.com>
2023-01-31 23:04:26 +0800

    Added tailwind setup

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,7 +1,8 @@
 module.exports = {
-    semi: false,
-    trailingComma: 'es5',
-    singleQuote: true,
-    tabWidth: 2,
-    useTabs: false,
-  }
+  plugins: [require('prettier-plugin-tailwindcss')],
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 function App() {
-  return <>Hello World</>
+  return (
+    <h1 className="text-3xl font-bold text-red-500 underline">Hello world!</h1>
+  )
 }
 
 export default App
